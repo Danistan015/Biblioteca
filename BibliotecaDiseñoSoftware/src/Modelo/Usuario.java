f5c9884bbd448d57bcdf62e5858c86b7e878a52c
Valeria <05valarist@gmail.com>
2023-08-26 09:16:32 -0500

    clase usuario creada

---
+++ BibliotecaDiseñoSoftware/src/Modelo/Usuario.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author val
+ */
+public class Usuario {
+        private String nombre;
+    private String cedula;
+    private int edad;
+    private String telefono;
+    private String correo;
+    private String contrasenia;
+
+    public Usuario(String nombre, String cedula, int edad, String telefono, String correo, String contrasenia) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+}
